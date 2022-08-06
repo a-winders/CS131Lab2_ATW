3dec98425d48cdba2165e89f67c93c3587b5aa4f
a-winders <awinders@bellarmine.edu>
2022-08-06 01:03:42 -0400

    revised submission

---
+++ CS131Lab2_ATW/src/Lockable.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ * @author Adam Winders
+ *
+ *put javadoc fields here
+ */
+
+
+public interface Lockable {
+	
+	public void setKey(int key);
+	// end setKey
+	
+	public void lock (int key);
+	// end lock
+	
+	public void unlock(int key);
+	// end unlock
+	
+	public boolean isLocked ();
+	// end isLocked
+	
+} // end class	
+
+
